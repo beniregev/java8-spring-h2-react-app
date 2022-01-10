e56022297d3bdd8c160269cfacb3c158132bd1b3
beni.regev@finastra.com <qe7uwt2v!>
2022-01-10 13:32:26 +0200

    Initial Version - 1st commit (IntelliJ IDEA)

---
+++ src/main/java/com/beniregev/java8springh2reactapp/Java8springh2reactappApplication.java
@@ -0,0 +1,13 @@
+package com.beniregev.java8springh2reactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8springh2reactappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8springh2reactappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beniregev/java8springh2reactapp/Java8springh2reactappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beniregev.java8springh2reactapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8springh2reactappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
