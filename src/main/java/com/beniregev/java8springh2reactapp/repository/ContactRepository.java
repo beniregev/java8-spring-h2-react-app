1fdc981f35768311a7746029b742f043a1b5173a
beni.regev@finastra.com <qe7uwt2v!>
2022-01-10 23:02:20 +0200

    Adding repository ContactRepository class

---
+++ src/main/java/com/beniregev/java8springh2reactapp/repository/ContactRepository.java
@@ -0,0 +1,9 @@
+package com.beniregev.java8springh2reactapp.repository;
+
+import com.beniregev.java8springh2reactapp.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+}
