f19abdba164d6724f0c2023d7fa49716e780a4dd
beni.regev@finastra.com <qe7uwt2v!>
2022-01-16 21:50:26 +0200

    NOT TESTED - Adding some files modifications, NOT TESTED, may need to undo them

--- src/main/java/com/beniregev/java8springh2reactapp/model/DemoLoader.java
+++ src/main/java/com/beniregev/java8springh2reactapp/model/DemoLoader.java
@@ -1,22 +1,22 @@
 package com.beniregev.java8springh2reactapp.model;
 
 import com.beniregev.java8springh2reactapp.repository.ContactRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DemoLoader implements CommandLineRunner {
 
-    private final ContactRepository contactRepository;
+    private ContactRepository contactRepository;
 
-    @Autowired
     public DemoLoader(ContactRepository contactRepository) {
         this.contactRepository = contactRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        this.contactRepository.save(new Contact("King", "David", "king.david@bible.com"));
+        contactRepository.save(new Contact("King", "David", "king.david@bible.com"));
     }
+
 }
